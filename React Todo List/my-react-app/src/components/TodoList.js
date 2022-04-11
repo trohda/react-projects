cb8bf985c825db8a00b85fb85d4bf30cd4e4457e
Tomasz Rohda <tomaszrohda@wp.pl>
2022-04-11 21:17:20 +0200

    little progress

--- React Todo List/my-react-app/src/components/TodoList.js
+++ React Todo List/my-react-app/src/components/TodoList.js
@@ -1,7 +1,26 @@
+import {useEffect, useState} from 'react';
+
 const TodoList = () => {
+
+    const [toDos, setToDos] = useState('');
+
+    useEffect(()=>{
+    fetch('http://localhost:8000/todo')
+    .then(res=> {return res.json()}
+    )
+    .then(data=>{
+        setToDos(data)})},[])
     return ( 
         <>
-            Todo list
+        {
+            toDos.map(el=>{
+                return(
+                    <div key={el.id}>
+                        <p>{el.name}</p>
+                    </div>
+                )
+            })
+        }
         </>
      );
 }
