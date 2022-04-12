f83cd6af8abb3a3f4421e31ab7a043cf7dc10600
Tomasz Rohda <tomaszrohda@wp.pl>
2022-04-12 20:07:07 +0200

    React Todo List - Challenge 3 - Done

--- React Todo List/my-react-app/src/components/TodoList.js
+++ React Todo List/my-react-app/src/components/TodoList.js
@@ -1,28 +1,26 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from "react";
 
 const TodoList = () => {
+  const [toDos, setToDos] = useState([]);
 
-    const [toDos, setToDos] = useState('');
+  useEffect(() => {
+    fetch("http://localhost:8000/todo")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setToDos(data);
+      });
+  }, []);
+  return (
+    <>
+      {toDos.map((el) => (
+        <div key={el.id}>
+          <p>{el.name}</p>
+        </div>
+      ))}
+    </>
+  );
+};
 
-    useEffect(()=>{
-    fetch('http://localhost:8000/todo')
-    .then(res=> {return res.json()}
-    )
-    .then(data=>{
-        setToDos(data)})},[])
-    return ( 
-        <>
-        {
-            toDos.map(el=>{
-                return(
-                    <div key={el.id}>
-                        <p>{el.name}</p>
-                    </div>
-                )
-            })
-        }
-        </>
-     );
-}
- 
 export default TodoList;
